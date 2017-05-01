e9729c6aeb39ad73f7e483ded4bb0bc272b06039
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-04-30 20:11:12 -0400

    Lit

--- BinaryTree/src/binarytree/BTClass.java
+++ BinaryTree/src/binarytree/BTClass.java
@@ -2,7 +2,40 @@ package binarytree;
 
 public class BTClass {
 
-	public static void main (String args[]){
-		System.out.println("GitHub pls");
-	}
+	BTClass left, right;
+    int data;
+    
+    public BTClass(){
+        left = null;
+        right = null;
+        data = 0;
+    }
+    public BTClass(int n){
+        left = null;
+        right = null;
+        data = n;
+    }
+    public void setLeft(BTClass n){
+        left = n;
+    }
+
+    public void setRight(BTClass n){
+        right = n;
+    }
+
+    public BTClass getLeft(){
+        return left;
+    }
+
+    public BTClass getRight(){
+        return right;
+    }
+
+    public void setData(int d){
+        data = d;
+    }
+
+    public int getData(){
+        return data;
+    }   
 }
---
+++ BinaryTree/src/binarytree/BinaryTree.java
@@ -0,0 +1,16 @@
+package binarytree;
+
+public class BinaryTree {
+
+	private BTClass root;
+	
+	public BinaryTree(){
+        root = null;
+    }
+	public boolean search(){
+		if(root != null){
+			return root != null;
+		}
+		return root == null;
+	}
+}
