3a9b44cc873888156988e24a1d694cec7f8e93b9
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-04-30 20:15:36 -0400

    Insert done my guy

--- BinaryTree/src/binarytree/BinaryTree.java
+++ BinaryTree/src/binarytree/BinaryTree.java
@@ -13,4 +13,23 @@ public class BinaryTree {
 		}
 		return root == null;
 	}
+	public void insert(int data){
+		insert(root, data);
+	}
+	private void insert(BTClass node, int data){
+		if (data <= node.getData()){
+    		if(node.left != null){
+    			insert(node.left, data);
+    	}else{
+    		node.left = new BTClass(data);
+    }
+    	}else if(data > node.getData()){
+    		if( node.right != null){
+    			insert(node.right, data);
+    		}else{
+    			node.right = new BTClass(data);
+    		}
+    	}
+    }
+	
 }
