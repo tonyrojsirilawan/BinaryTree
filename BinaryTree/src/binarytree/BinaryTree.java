65897b78184ddf8d212642f82d7f1c1ae5837ded
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-05-07 15:20:56 -0400

    Rest of the methods except for word file reading one

--- BinaryTree/src/binarytree/BinaryTree.java
+++ BinaryTree/src/binarytree/BinaryTree.java
@@ -1,9 +1,11 @@
 package binarytree;
 
+import java.util.ArrayList;
+
 public class BinaryTree {
 
 	private BTClass root;
-	
+	ArrayList<Integer> tree = new ArrayList<Integer>();
 	public BinaryTree(){
         root = null;
     }
@@ -24,12 +26,32 @@ public class BinaryTree {
     		node.left = new BTClass(data);
     }
     	}else if(data > node.getData()){
-    		if( node.right != null){
+    		if(node.right != null){
     			insert(node.right, data);
     		}else{
     			node.right = new BTClass(data);
     		}
     	}
     }
-	
+	public void delete(int data){
+		delete(root, data);
+	}
+	private void delete(BTClass node, int data){
+		if (root == null){
+			new RuntimeException("Nothing in here fam");
+		}else{
+			node.left = node.right;
+		}
+	}
+	public void inorder(){
+		inorder(root);
+	}
+	private void inorder(BTClass r){
+		if (r != null){
+			inorder(r.getLeft());
+			System.out.print(r.getData() + " ");
+			tree.add(r.getData());
+			inorder(r.getRight());
+		}
+	}
 }
